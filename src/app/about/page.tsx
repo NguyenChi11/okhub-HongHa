eabda478c1f342e3fb76c7ecb1c0d96b485ee34d
baodo1101 <baodhq.yec22@gmail.com>
2025-06-19 14:22:55 +0700

    create primary tourguide

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import React from 'react'
 import styles from "./custom.module.css"
 import AboutHeader from '../components/AboutHeader/AboutHeader'
 import Breadcrumb from '../components/Breadcrumb/Breadcrumb'
 import AboutBanner from '../components/AboutBanner/AboutBanner'
 import AboutAnimation from '../components/AboutAnimation/AboutAnimation'
+import PrimaryTourGuide from '../components/ReviewHome/components/PrimaryTourGuide'
+
 
 const page = () => {
 
@@ -23,14 +27,30 @@ const page = () => {
     description:
       "At our Ha Giang tourism company, we adhere to a set of ethical guidelines that guide our operations and define our commitment to responsible tourism. Our foremost principle is to respect and preserve the natural environment and cultural heritage of Ha Giang. We prioritize sustainable practices to minimize our ecological footprint and actively engage in conservation efforts. Furthermore, we deeply value the communities we operate in and strive to foster positive relationships with local residents. We prioritize their well-being and economic empowerment through fair employment practices and community development initiatives. Additionally, we prioritize the safety and satisfaction of our guests, ensuring that every experience with us is both enjoyable and enriching",
   };
+
+  const TourGuide_data = {
+    text: "HONG HAO TRAVEL",
+    title: "OUR TEAM",
+    description:
+      "We pride ourselves on having a team of dedicated and passionate individuals who are committed to providing exceptional service and unforgettable experiences to our guests. Our team is comprised of knowledgeable professionals with diverse backgrounds and expertise in various aspects of the tourism industry.",
+  };
   return (
     <div className={`${styles.container}`}>
-        <AboutHeader text1={textAbout.text1} text2={textAbout.text2}/>
-        <Breadcrumb/>
-        <AboutBanner text={TextBannerAbout.text1}/>
-        <AboutAnimation text={TextAboutAnimation.text1} title={TextAboutAnimation.title1} description={TextAboutAnimation.description}/>
+      <AboutHeader text1={textAbout.text1} text2={textAbout.text2} />
+      <Breadcrumb />
+      <AboutBanner text={TextBannerAbout.text1} />
+      <AboutAnimation
+        text={TextAboutAnimation.text1}
+        title={TextAboutAnimation.title1}
+        description={TextAboutAnimation.description}
+      />
+      <PrimaryTourGuide
+        text={TourGuide_data.text}
+        title={TourGuide_data.title}
+        description={TourGuide_data.description}
+      />
     </div>
-  )
+  );
 }
 
 export default page
